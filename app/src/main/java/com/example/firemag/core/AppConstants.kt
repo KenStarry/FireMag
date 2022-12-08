24e80797e74f5add10388460bdae5cea80398ca5
KenStarry <starrycodes@gmail.com>
2022-12-08 18:43:12 +0300

    Finished AppConstants

--- app/src/main/java/com/example/firemag/core/AppConstants.kt
+++ app/src/main/java/com/example/firemag/core/AppConstants.kt
@@ -26,6 +26,46 @@ object AppConstants {
     const val ADD_TO_CART = "Add To Cart"
     const val SEND_ORDER = "Send order"
     const val BACK_TO_HOME = "Back to Home"
+
+    //  Menu Items
+    const val HOME = "Home"
+    const val ORDERS = "Orders"
+    const val FAVOURITES = "Favourites"
+    const val SIGN_OUT = "Sign-out"
+
+    //Titles
+    const val BRANDS_TITLE = "Brands:"
+    const val POPULAR_PRODUCTS_TITLE = "Popular Products:"
+    const val FAVORITE_PRODUCTS_TITLE = "Favorite Products:"
+    const val SHOES = "shoes"
+
+    //Image Extensions
+    const val PNG = "png"
+
+    //Placeholders
+    const val SEARCH = "Search..."
+
+    //Empty Value
+    const val NO_VALUE = ""
+
+    //Arguments
+    const val PRODUCT_BRAND = "productBrand"
+    const val ORDER_ID = "orderId"
+    const val PRODUCT_ID = "productId"
+
+    //Messages
+    const val NO_PRODUCTS_FOUND_MESSAGE = "No products found."
+    const val EMPTY_CART_MESSAGE = "Your cart is empty."
+    const val THANK_YOU_MESSAGE = "Thank you for your order."
+
+    //Texts
+    const val TOTAL_PAYMENT = "Total Payment"
+    const val PAID = "Paid"
+
+    //Symbols
+    const val MINUS = "-"
+    const val PLUS = "+"
+    const val DOT = '.'
 }
 
 
