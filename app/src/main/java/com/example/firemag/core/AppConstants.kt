ae16e117ad9e9b7817ff2c49f5003fe8722e86e9
KenStarry <starrycodes@gmail.com>
2022-12-08 18:38:56 +0300

    FireMag

---
+++ app/src/main/java/com/example/firemag/core/AppConstants.kt
@@ -0,0 +1,45 @@
+package com.example.firemag.core
+
+object AppConstants {
+    //  App
+    const val TAG = "FireMagTag"
+
+    //  Screens
+    const val AUTH_SCREEN = "Authentication"
+    const val MAIN_SCREEN = "Main"
+    const val PRODUCT_SEARCH_SCREEN = "Product Search"
+    const val SHOPPING_CART_SCREEN = "Shopping Cart"
+    const val BRAND_PRODUCTS_SCREEN = "Brand Products"
+    const val PRODUCT_DETAILS_SCREEN = "Product Details"
+    const val PRODUCTS_ORDER_SCREEN = "Products Order"
+    const val THANK_YOU_SCREEN = "Thank You"
+
+    //  Currency
+    const val US_DOLLAR = "$"
+
+    //  Names
+    const val SIGN_IN_REQUEST = "signInRequest"
+    const val SIGN_UP_REQUEST = "signUpRequest"
+
+    //  Buttons
+    const val SIGN_IN_WITH_GOOGLE = "Sign In With Google"
+    const val ADD_TO_CART = "Add To Cart"
+    const val SEND_ORDER = "Send order"
+    const val BACK_TO_HOME = "Back to Home"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
